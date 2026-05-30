56a14433a04c0d47cb61543f90b8298754edf90e
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 13:10:46 +0200

    Ajuste mais um até funcionar

--- src/components/pulse_tech/requisicao/Requisicao.js
+++ src/components/pulse_tech/requisicao/Requisicao.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Noticia from '../sessao/Noticia';
+// import Noticia from '../sessao/Noticia';
 
 const Requisicao = () => {
 
@@ -34,7 +34,7 @@ const Requisicao = () => {
 
     return (
         <>
-            <p></p>
+            <p>{noticias}</p>
             {/* {noticias?.slice(0, 18).map((noticia, id_noticia) => (<Noticia id={id_noticia} noticiaImg={noticia.image || noticia.image_url || noticia.urlToImage} noticaAlt={noticia.title} noticiaTitulo={noticia.title} noticiaDesc={noticia.description} noticiaLerMais={noticia.url || noticia.link} />))} */}
         </>);
 };
