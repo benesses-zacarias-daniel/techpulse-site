1e97163d6974556e328f90c050b1ab5d6596c854
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 17:48:46 +0200

    Ajuste da função de noticias locais

--- services/noticia_local.js
+++ services/noticia_local.js
@@ -1,10 +1,8 @@
-import ImagemPadrao from "../src/components/assets/Logo3LoginSF.png";
-
 const noticiaLocal = () => [
     {
         title: "Sem notícias no momento",
         description: "As APIs externas falharam, mas o sistema continua ativo.",
-        image: ImagemPadrao,
+        image: "",
         url: "https://techpulsesite.vercel.app/",
         category: "Sistema"
     }
