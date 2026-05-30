6f41baf2fc003202156054adee121341c8f467be
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 13:21:16 +0200

    Outro ajuste ate dar certo

--- api/api_noticias.js
+++ api/api_noticias.js
@@ -3,10 +3,14 @@ const TrazerNoticias = async (requisicao, resposta) => {
         console.log("Trazer Noticias");
 
         const chaveGnews = process.env.GNEWS_API_KEY;
-        const req = await fetch(`https://gnews.io/api/v4/search?q=technology OR artificial intelligence OR programming OR cybersecurity OR software OR hardware OR Operating System OR Sistema Operativo OR ia -agriculture -farming&lang=pt&max=18&apikey=${chaveGnews}`);
-        const noticias = await req.json();
+        console.log(chaveGnews);
 
-        return resposta.status(200).json(noticias);
+        // const req = await fetch(`https://gnews.io/api/v4/search?q=technology OR artificial intelligence OR programming OR cybersecurity OR software OR hardware OR Operating System OR Sistema Operativo OR ia -agriculture -farming&lang=pt&max=18&apikey=${chaveGnews}`);
+        // const noticias = await req.json();
+
+        return resposta.status(200).json({
+            ok: true
+        });
     } catch (error) {
         resposta.status(200).json({ erro: "Erro ao buscar Notícias" });
     }
