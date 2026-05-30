ea3fb626e2ac996d35815138e63dd6de7915f523
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 16:10:37 +0200

    Chave da GNews ajustada e funcioanl

--- api/api_noticias.js
+++ api/api_noticias.js
@@ -5,7 +5,7 @@ const TrazerNoticias = async (requisicao, resposta) => {
         const chaveGnews = process.env.GNEWS_API_KEY;
         console.log(chaveGnews);
 
-        const req = await fetch(`https://gnews.io/api/v4/search?q=technology OR artificial intelligence OR programming OR cybersecurity OR software OR hardware OR Operating System OR Sistema Operativo OR ia -agriculture -farming&lang=pt&max=18&apikey=${chaveGnews}`);
+        const req = await fetch(`https://gnews.io/api/v4/search?q=technology%20artificial%20intelligence%20programming%20cybersecurity%20software%20hardware%20Operating%20System%20Sistema%20Operativo%20ia&lang=pt&max=18&apikey=${chaveGnews}`);
         const noticias = await req.json();
 
         if (!noticias || !noticias.articles) {
