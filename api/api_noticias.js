ee425fbe29a1a41092d226d183616045a72475ce
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 11:56:51 +0200

    No ultimo tinha removido algo e devolvi agora

--- api/api_noticias.js
+++ api/api_noticias.js
@@ -1,4 +1,4 @@
-const TrazerNoticias = async () => {
+const TrazerNoticias = async (requisicao, resposta) => {
     try {
         const chaveGnews = process.env.GNEWS_API_KEY;
         const req = await fetch(`https://gnews.io/api/v4/search?q=technology OR artificial intelligence OR programming OR cybersecurity OR software OR hardware OR Operating System OR Sistema Operativo OR ia -agriculture -farming&lang=pt&max=18&token=${chaveGnews}`);
