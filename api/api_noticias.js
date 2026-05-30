88200b1cd9632e992b8936d61e81e3e5f5933feb
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 14:36:37 +0200

    Ajuste da conversão

--- api/api_noticias.js
+++ api/api_noticias.js
@@ -6,7 +6,7 @@ const TrazerNoticias = async (requisicao, resposta) => {
         console.log(chaveGnews);
 
         const req = await fetch(`https://gnews.io/api/v4/search?q=technology OR artificial intelligence OR programming OR cybersecurity OR software OR hardware OR Operating System OR Sistema Operativo OR ia -agriculture -farming&lang=pt&max=18&apikey=${chaveGnews}`);
-        const noticias = await req.json(req);
+        const noticias = await req.json();
 
         if (!noticias || !noticias.articles) {
             return resposta.status(200).json({
