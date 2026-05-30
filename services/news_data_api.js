caabdcf7b60336445a4a378c5e29f908bd1892a2
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 19:27:24 +0200

    Ajuste da vericação do retorno

--- services/news_data_api.js
+++ services/news_data_api.js
@@ -10,7 +10,7 @@ const newsDataAPI = async (chaves) => {
 
     console.log(data);
 
-    if (!data || !Array.isArray(data.results)) throw new Error("NewsData vazio");
+    if (!Array.isArray(data.results) || data.results.length === 0) throw new Error("NewsData vazio");
 
     return data.results.map(artigo => ({
         title: artigo.title,
