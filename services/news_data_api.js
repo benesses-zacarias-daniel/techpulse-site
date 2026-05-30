204e649dfb1046e70ce411128d4496c3402287b3
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-05-30 21:44:32 +0200

    Ajuste de filtro do news data api

--- services/news_data_api.js
+++ services/news_data_api.js
@@ -2,7 +2,7 @@ import { NormalizarNoticial } from "../utils/normalizar_noticia";
 import Categoria from "../utils/categoria";
 
 const newsDataAPI = async (chaves) => {
-    const requisicao = await fetch(`https://newsdata.io/api/1/news?apikey=${chaves.chaveNewsData}&size=18&q=technology OR ia OR programming OR cybersecurity OR software OR hardware OR Operating System&language=pt`);
+    const requisicao = await fetch(`https://newsdata.io/api/1/news?apikey=${chaves.chaveNewsData}&q=technology OR ia OR programming OR cybersecurity OR software OR hardware OR Operating System&language=pt`);
 
     const resposta = await requisicao.json();
 
