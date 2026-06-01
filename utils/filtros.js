131029a6deec018973d516daaebc4406e30d05be
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-01 07:55:48 +0200

    Ajuste descricao

--- utils/filtros.js
+++ utils/filtros.js
@@ -56,7 +56,7 @@ const temImagem = (artigo) => {
 
 const bloquear = (artigo) => {
     console.log("Entrou Bloquear");
-    const texto = `${artigo.titulo} ${artigo.descriacao}`.toLowerCase();
+    const texto = `${artigo.titulo} ${artigo.descricao}`.toLowerCase();
     return !noticiasBloquear.some(palavra => texto.includes(palavra));
 }
 
