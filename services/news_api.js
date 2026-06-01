f22d66b86266a29528dc9d205e94b1744e5eb32d
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-01 08:02:48 +0200

    Ajuste nr de noticia

--- services/news_api.js
+++ services/news_api.js
@@ -2,7 +2,7 @@ import { bloquear, temImagem } from "../utils/filtros";
 import NormalizarNoticial from "../utils/normalizar_noticia";
 
 const newsAPI = async (chaves) => {
-    const requisicao = await fetch(`https://newsapi.org/v2/top-headlines?q=technology&pageSize=18&apiKey=${chaves.chaveNewsAPI}`);
+    const requisicao = await fetch(`https://newsapi.org/v2/top-headlines?q=technology&apiKey=${chaves.chaveNewsAPI}`);
 
     const resposta = await requisicao.json();
     console.log("Retorno do NewsAPI");
