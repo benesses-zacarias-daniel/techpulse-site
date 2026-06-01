bf29dd607412a8e365e63fa431cb411f1046c147
Benesses Zacarias Daniel <benezeszdaniel@gmail.com>
2026-06-01 09:24:46 +0200

    Ajustes de formas de pesquisa e retorno das respostas como tambem a forma dos caminhos

--- api/api_noticias.js
+++ api/api_noticias.js
@@ -4,9 +4,9 @@ import newsDataAPI from "../services/news_data_api";
 import noticiaLocal from "../services/noticia_local";
 
 const funcoesNoticias = [
+    newsDataAPI,
     gNewsAPI,
-    newsAPI,
-    newsDataAPI
+    newsAPI
 ];
 
 const TrazerNoticias = async (requisicao, resposta) => {
--- services/news_data_api.js
+++ services/news_data_api.js
@@ -3,14 +3,13 @@ import Categoria from "../utils/categoria";
 import { bloquear, limitarDescricao, noticiaTech, temImagem } from "../utils/filtros";
 
 const newsDataAPI = async (chaves) => {
-    const requisicao = await fetch(`https://newsdata.io/api/1/news?apikey=${chaves.chaveNewsData}&q=technology&language=pt`);
-
+    const requisicao = await fetch(`https://newsdata.io/api/1/latest?apikey=${chaves.chaveNewsData}&country=us,at,br,cn,jp&language=pt&category=technology%20%20&image=1%20%20&removeduplicate=1%20%20&sort=relevancy%20%20&size=18&q=technology%20OR%20programming%20OR%20cybersecurity%20OR%20software%20OR%20hardware%20OR%20Operating%20System%20OR%20ia`);
+    //https://newsdata.io/api/1/latest?apikey=${chaves.chaveNewsData}&country=us,at,br,cn,jp&language=pt&category=technology%20%20&image=1%20%20&removeduplicate=1%20%20&sort=relevancy%20%20&size=10&q=technology%20OR%20programming%20OR%20cybersecurity%20OR%20software%20OR%20hardware%20OR%20Operating%20System%20OR%20ia
     const resposta = await requisicao.json();
     console.log("Retorno da news data");
     console.log(resposta);
     if (!Array.isArray(resposta.results) || resposta.results.length === 0) { throw new Error("NewsData vazio") };
     const dadosFinais = resposta.results.map(artigo => NormalizarNoticial(artigo)).filter(artigo => temImagem(artigo)).filter(artigo => bloquear(artigo)).filter(artigo => noticiaTech(artigo)).filter(artigo => limitarDescricao(artigo));
-
     return dadosFinais;
 };
 
